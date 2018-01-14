bb91e14b79f937537c38a3796e405beeedca5a26
George H. Peden <georgep@slalom.com>
2018-01-14 20:04:55 +0000

    enable icmp so bastion can ping internet hosts

--- tf/main.tf
+++ tf/main.tf
@@ -74,6 +74,20 @@ resource "aws_security_group" "georgep-challenge-sg-bastion" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 8
+    to_port = 0
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   vpc_id = "${aws_vpc.vpc.id}"
 
 }
